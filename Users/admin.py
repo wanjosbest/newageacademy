d826a41394066053c92289a234a603887abbdd05
wanjosbest <best9ja1@gmail.com>
2025-01-29 03:19:12 -0800

    Update admin.py

--- Users/admin.py
+++ Users/admin.py
@@ -1,3 +1,4 @@
+"""
 from django.contrib import admin
 
 from .models import (User,available_Courses,liveclass,studentatten,anouncement,CreateAssignment,coursetimetable,
@@ -24,6 +25,7 @@ admin.site.register(coursemodule)
 admin.site.register(examtimetable)
 admin.site.register(Transaction)
 admin.site.register(promotedcourses)
+"""
 admin.site.register(Referral)
 
 
